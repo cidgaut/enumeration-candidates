9d4e926e2f4a116940450cdf2b943b94c5f9efdc
Cedric.gauthier <cidgaut@gmail.com>
2024-02-29 16:15:12 -0500

    added methods in filters for candidate classifications and process of elimination.

--- filters.rb
+++ filters.rb
@@ -2,15 +2,25 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+    @candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    #adding each one by one via process of elimination.
+    experienced_candidates = candidates.select { |candidate| experienced?(candidate) }
+    github_points_candidates = experienced_candidates.select { |candidate| candidate[:github_points] >= 100 }
+    ruby_or_python_candidates = github_points_candidates.select { |candidate| candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python') }
+    recent_applicants = ruby_or_python_candidates.select { |candidate| candidate[:date_applied] >= 15.days.ago.to_date }
+    adult_candidates = recent_applicants.select { |candidate| candidate[:age] > 17 }
+
+    adult_candidates
   end
   
-  # More methods will go below
+  #return all candidates but list them via years of experience and again by github points.
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
